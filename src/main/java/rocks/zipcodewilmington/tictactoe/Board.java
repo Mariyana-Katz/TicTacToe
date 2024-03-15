9308ee6748dc1849e878d87fefa78e4fc9d5e6a0
Mariyana-Katz <mariqnak21@gmail.com>
2024-03-15 14:40:47 -0400

    finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,70 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+    Character[][] gameBoard;
+    public  Board(Character[][] matrix) {
+        this.gameBoard = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        if(getWinner()=="X" ){
+            return true;
+        }
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if(getWinner()== "O") {
+            return true;
+        }
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if(getWinner()==""){
+            return true;
+        }
+        else
+            return false;
     }
 
     public String getWinner() {
-        return null;
+        if (checkWinner('X')) {
+            return "X";
+
+        } else if (checkWinner('O')) {
+            return "O";
+        }
+        else {
+            return "";
+        }
+
     }
 
-}
+
+    public Boolean checkWinner(char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (gameBoard[i][0] == symbol && gameBoard[i][1] == symbol && gameBoard[i][2] == symbol) {
+                return true;
+            }
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (gameBoard[0][j] == symbol && gameBoard[1][j] == symbol && gameBoard[2][j] == symbol) {
+                return true;
+            }
+        }
+
+        if ((gameBoard[0][0] == symbol && gameBoard[1][1] == symbol && gameBoard[2][2] == symbol)) {
+            return true;
+        }
+        else if ((gameBoard[0][2] == symbol && gameBoard[1][1] == symbol && gameBoard[2][0] == symbol)) {
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+
+    }
